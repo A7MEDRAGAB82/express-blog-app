c6f172e21e99bb8aec345be9563d57824bed36dd
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-12-30 01:47:59 +0200

    Add MySQL connection and test endpoint

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2/promise");
+const app = express();
+const port = 3000;
 
-app.use(express.json)
+app.use(express.json());
 
+const databaseConnection = async () => {
+  await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+}).then(() => {
+      console.log("datbase connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
+databaseConnection()
 
-app.listen(port,()=>console.log('Example app listening at http://localhost:3000'));
+app.listen(port, () =>
+  console.log("Example app listening at http://localhost:3000")
+);
