8ff64c3f60b4f5b4020982c8eaced9700c97c703
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-12-30 22:49:45 +0200

    add login

--- index.js
+++ index.js
@@ -1,26 +1,65 @@
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const databaseConnection = async () => {
-  await mysql.createConnection({
+const Connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-}).then(() => {
-      console.log("datbase connected");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+});
 
-databaseConnection()
+Connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("database connected");
+  }
+});
+
+app.post("/auth/signup", (req, res) => {
+  let { name, email, password } = req.body;
+
+  Connection.query(
+    `INSERT INTO users (name,email,password) values ('${name}','${email}','${password}')`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      else if (result.affectedRows > 0) {
+        res.json({message: "user registered successfully"})
+      }
+      else {
+        res.status(500).json({message: "user registiration failed"})
+      }
+    }
+  );
+});
+
+app.post('/auth/login', (req,res)=>{
+  let {email , password} = req.body;
+
+  Connection.query(`SELECT * from users WHERE email = '${email}' and password = '${password}'`,(err,result)=>{
+        if(err){
+          console.log(err);
+          res.json({message: "internal server error"})
+          return
+        }
+        else if (result.length > 0) {
+           res.json({message: " login successfully",result})
+       
+        }
+        else {
+          res.json({message: "invalid credintials"})
+        }
+  } )
+
+
+})
 
 app.listen(port, () =>
   console.log("Example app listening at http://localhost:3000")
